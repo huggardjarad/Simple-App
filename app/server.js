d1d1573262f172e312c4a9872549eb96c24fe60d
Jarad Huggard <j.huggard@techmodal.com>
2021-12-02 16:03:56 +0000

    Changing port number

--- app/server.js
+++ app/server.js
@@ -1,5 +1,5 @@
 var app = require('./simple-app/app');
-var port = process.env.PORT || 8000;
+var port = process.env.PORT || 8080;
 
 app.listen(port, function() {
     console.log(`Server started, listening on port ${port}`);
